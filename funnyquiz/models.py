fe1736f7b7da9035cca0819f6ace0191bafa8534
HesterG <hestergong@gmail.com>
2016-12-12 00:32:15 -0500

    Update

--- funnyquiz/models.py
+++ funnyquiz/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
+class Quiz(models.Model):
+    QuizName = models.CharField(max_length=50)
+    QuizUrl = models.CharField(max_length=200)
+    UseStandardWelcomePage = models.BooleanField(default=True)
+    WelcomeMessage = models.CharField(max_length=200)
+    WelcomeImage = models.FileField(null=True, blank=True)
+    UseStandardResultPage = models.BooleanField(default=True)
+    CreatedTime = models.DateField()
+
+    def __str__(self):
+        return str(self.pk) + ' - ' + str(self.QuizName)
+
+
+class Question(models.Model):
+    Quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    QuestionText = models.CharField(max_length=200)
+    '''QuestionHasImage = models.BooleanField(default=False)'''
+    QuestionImage = models.FileField(null=True, blank=True)
+    
+
+    def __str__(self):
+        return str(self.pk) + ' - ' + self.QuestionText
+
+
+class Answer(models.Model):
+    ParentQuestion = models.ForeignKey(Question, on_delete=models.CASCADE)
+    AnswerText = models.CharField(max_length=100)
+    AnswerImg = models.FileField(null=True, blank=True)
+    '''AnswerHasImage = models.BooleanField(default=False)'''
+    IsCorrectAnswer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.AnswerText
+
+class Quizstate(models.Model):
+    Quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    VisitorCount = models.IntegerField()
+    CompletionCount = models.IntegerField()
+    AvgCorrecteness = models.FloatField()
+    StdDev = models.FloatField()
+
+
+
--- funnyquiz/urls.py
+++ funnyquiz/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'funnyquiz'
 urlpatterns = [
 	url(r'^$', views.quizList, name='quiz_index'),
-	url(r'^(?P<quiz_url>[^/]+)/$', views.quizWelcome, name='quiz_welcome')
-	
+	url(r'^(?P<quiz_url>[^/]+)/$', views.quizWelcome, name='quiz_welcome'),
+    url(r'^(?P<quiz_url>[^/]+)/result/$', views.quizResult,
+		name='quiz_result'),
 ]
